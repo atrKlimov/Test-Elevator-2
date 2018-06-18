e66ae270ee28158c4ac91fd9cd87a5fb97c37e25
atrKlimov <34440537+atrKlimov@users.noreply.github.com>
2018-06-18 21:09:42 +0300

    Initial Commit

---
+++ Test Elevator 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Elevator 2
+//
+//  Created by Artem Klimov on 18.06.2018.
+//  Copyright © 2018 Artem Klimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
